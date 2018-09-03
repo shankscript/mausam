d6a544242c44fc7ff625a7f765f99ff090929ab6
shashsh2 <shashsh2@cisco.com>
2018-09-03 13:37:48 +0530

    init

---
+++ app/src/main/java/me/shankscript1/mausam/tasks/ParseResult.java
@@ -0,0 +1,3 @@
+package me.shankscript1.mausam.tasks;
+
+public enum ParseResult {OK, JSON_EXCEPTION, CITY_NOT_FOUND}
---
+++ app/src/main/java/me/shankscript1/mausam/tasks/TaskResult.java
@@ -0,0 +1,3 @@
+package me.shankscript1.mausam.tasks;
+
+public enum TaskResult { SUCCESS, BAD_RESPONSE, IO_EXCEPTION, TOO_MANY_REQUESTS; }
